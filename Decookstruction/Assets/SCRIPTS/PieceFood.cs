03de4d9c7bd34e6579a2a9c26b28706504dcdce2
KanedaXIII <mnemosineii@gmail.com>
2015-10-23 23:06:39 +0200

    Creado escena game
    -Creada escena game.
    -Creado script pieceFood.

---
+++ Decookstruction/Assets/SCRIPTS/PieceFood.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PieceFood : MonoBehaviour {
+
+    public string namePiece;
+
+    public bool drag;
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseDrag()
+    {
+        transform.position = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, 21f));
+    }
+    
+    
+}
