e5b9663c0c3aec94c458f0c19933ffefd526c99e
LuisPalomo <luispalomosiv@gmail.com>
2015-10-24 10:26:39 +0200

    Implementado el GameManager y su Loader

--- Decookstruction/Assets/SCRIPTS/GameManager.cs
+++ Decookstruction/Assets/SCRIPTS/GameManager.cs
@@ -3,7 +3,9 @@ using System.Collections;
 
 public class GameManager : MonoBehaviour {
 
-    private static GameManager instance;
+	public static GameManager instance = null;
+
+	private int score;
 
     private GameManager() { }
 
@@ -20,8 +22,23 @@ public class GameManager : MonoBehaviour {
             return instance;
         }
     }
+
+	//Awake is always called before any Start functions
     void Awake()
     {
-        Debug.Log("Inicia");
+		//Check if instance already exists
+		if (instance == null)
+			
+			//if not, set instance to this
+			instance = this;
+		
+		//If instance already exists and it's not this:
+		else if (instance != this)
+			
+			//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+			Destroy(gameObject);    
+		
+		//Sets this to not be destroyed when reloading scene
+		DontDestroyOnLoad(gameObject);
     }
 }
