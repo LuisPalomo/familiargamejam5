6b5bfcc84a4b7bdfb0bdd770c9fde113091a2c2b
LuisPalomo <luispalomosiv@gmail.com>
2015-10-25 01:28:00 +0200

    Retoques

--- Decookstruction/Assets/SCRIPTS/FoodBucket.cs
+++ Decookstruction/Assets/SCRIPTS/FoodBucket.cs
@@ -14,6 +14,10 @@ public class FoodBucket : MonoBehaviour {
 
     public Text percentText;
 
+	public AudioClip drop;
+
+	public AudioClip dropError;
+
     string namePiece;
 
     GameObject dishAsso;
@@ -77,6 +81,8 @@ public class FoodBucket : MonoBehaviour {
         {
             Destroy(coll.gameObject);
 
+			SoundManager.instance.PlaySingle(drop);
+
             switch (nIngredient)
             {
                 case 1:
@@ -92,6 +98,7 @@ public class FoodBucket : MonoBehaviour {
         }
         else
         {
+			SoundManager.instance.PlaySingle(dropError);
             Destroy(coll.gameObject);
         }
        
