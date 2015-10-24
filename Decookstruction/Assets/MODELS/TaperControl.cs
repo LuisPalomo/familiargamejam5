7c219fb80f79f973d2d0a39a371b2b533b6fd6ba
Luis <lmgginspace@gmail.com>
2015-10-24 17:33:04 +0200

    Subido Taper
    Con animacion y todo bien hecha.

---
+++ Decookstruction/Assets/MODELS/TaperControl.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using UnityEngine;
+
+public class TaperControl : MonoBehaviour
+{
+    private Animator animator;
+
+    // Use this for initialization
+    private void Awake()
+    {
+        this.animator = this.GetComponent<Animator>();
+    }
+    /*
+    private void Update()
+    {
+        
+        if (Input.GetKey(KeyCode.A))
+            this.OpenTaper();
+
+        if (Input.GetKey(KeyCode.C))
+            this.CloseTaper();
+         
+    }
+    */
+    public void OpenTaper()
+    {
+        this.animator.SetBool("Abierto", true);
+    }
+
+    public void CloseTaper()
+    {
+        this.animator.SetBool("Abierto", false);
+    }
+}
