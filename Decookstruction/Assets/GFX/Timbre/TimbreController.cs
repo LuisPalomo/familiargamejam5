92d98fb1b4db0f19c552c525f36a07c93ed2e169
Luis <lmgginspace@gmail.com>
2015-10-24 20:07:43 +0200

    Timbre Hecho
    Timbre Hecho

---
+++ Decookstruction/Assets/GFX/Timbre/TimbreController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+
+public class TimbreController : MonoBehaviour
+{
+    public ParticleSystem noteSystem;
+    public ParticleSystem soundSystem;
+
+    public Transform bell;
+    public Transform button;
+
+    private float timeCounter = 0.0f;
+
+    private void Update()
+    {
+        if (Input.GetKeyUp(KeyCode.C))
+            this.GenerateSound();
+
+        if (this.timeCounter > 0.0f)
+        {
+            float effectMultiplier = timeCounter * 8.0f;
+            this.bell.localRotation = Quaternion.AngleAxis(Mathf.Sin(Time.time * 50.0f) * effectMultiplier, Vector3.forward);
+            this.timeCounter -= Time.deltaTime;
+        }
+
+        float buttonScale = Mathf.SmoothStep(this.button.localScale.y, 1.0f, 0.5f);
+        this.button.localScale = new Vector3(1.0f, buttonScale, 1.0f);
+	}
+
+    private void GenerateSound()
+    {
+        this.noteSystem.Emit(5);
+        this.soundSystem.Emit(20);
+
+        this.timeCounter = 1.0f;
+        this.button.localScale = new Vector3(1.0f, 0.33f, 1.0f);
+    }
+}
