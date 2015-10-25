495cd2b8b84ba628f952e63ab4a31d4a6a67d613
Luis <lmgginspace@gmail.com>
2015-10-25 11:33:19 +0100

    Instrucciones Main Menu

---
+++ Decookstruction/Assets/SCRIPTS/TitleWiggle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TitleWiggle : MonoBehaviour
+{
+    private float timeCounter = 0.0f;
+    private float wiggleTime = 0.0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.timeCounter += Time.deltaTime;
+        
+        if (this.timeCounter > 2.5f)
+        {
+            this.timeCounter -= 2.5f;
+            this.wiggleTime = 1.0f;
+        }
+
+        if (this.wiggleTime > 0.0f)
+            this.wiggleTime -= Time.deltaTime;
+        this.wiggleTime = (this.wiggleTime > 0.0f) ? this.wiggleTime : 0.0f;
+
+        this.Wiggle();
+    }
+
+    private void Wiggle()
+    {
+        float wiggleFactor = Mathf.Abs(0.5f - this.wiggleTime) * 2.0f;
+        this.transform.localRotation = Quaternion.AngleAxis(Mathf.Sin(Time.time) * 2.0f, Vector3.forward);
+        this.transform.localScale = new Vector3(1.0f + (Mathf.Sin(Time.time * 10.0f) * 0.01f), 1.0f + (Mathf.Sin(Time.time * 20.0f) * 0.01f), 1.0f);
+    }
+}
