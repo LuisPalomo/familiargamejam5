dc2ae1d8299ded610be32a5a85702a6908bb085f
Luis <lmgginspace@gmail.com>
2015-10-25 16:01:36 +0100

    Particulas estrellita

---
+++ Decookstruction/Assets/SCRIPTS/StarsController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StarsController : MonoBehaviour
+{
+    private ParticleSystem pSystem;
+
+    private void Awake()
+    {
+        this.pSystem = this.GetComponent<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    public void MakeStars()
+    {
+        if (this.pSystem != null)
+            this.pSystem.Emit(30);
+    }
+}
